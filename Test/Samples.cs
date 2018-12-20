1760587a17e461f4057fb3b63c71c59dedad7fb9
Tiny Rain <love@sandsea.info>
2018-12-21 01:05:14 +0800

    增强易用性和更容易的测试用例

--- Test/Samples.cs
+++ Test/Samples.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetDetour;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -7,36 +8,119 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    public class A
+    public class Computer
     {
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static string StaticMethod()
+        
+        public static string GetCpu()
         {
-            return "A";
+            return "Intel Core I7";
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public string InstanceMethod()
+        public string GetRAMSize()
         {
-            return "A";
+            return "512M";
         }
 
-        public string Property
+        internal string Os
         {
-            [MethodImpl(MethodImplOptions.NoInlining)]
             get
             {
-                return "A";
+                return "Windows 10";
             }
         }
     }
 
-    public class A1<T>
+
+    public class ComputerOf<T>
     {
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public T GenericMethod(T t)
+        public T ComputerIo(T owner)
         {
-            return t;
+            return owner;
+        }
+    }
+
+    public class ComputerDetour : IMethodHook
+    {
+        #region 静态方法HOOK
+        [RelocatedMethodAttribute(typeof(Computer), "GetCpu")]
+        public static string _impl_GetCpu()
+        {
+            return "Not " + GetCpu();
+        }
+        
+        [ShadowMethod(typeof(Computer), "GetCpu")]
+        public static string GetCpu()
+        {
+            return null;
+        }
+        #endregion
+
+        #region 实例方法
+        [RelocatedMethodAttribute(typeof(Computer), "GetRAMSize")]
+        public string _impl_GetRAMSize()
+        {
+            return "Not " + GetRAMSize();
+        }
+        
+        [ShadowMethod(typeof(Computer), "GetRAMSize")]
+        public string GetRAMSize()
+        {
+            return null;
+        }
+        #endregion
+
+
+        #region 实例属性
+        public string _impl_Os
+        {
+            [RelocatedMethodAttribute(typeof(Computer), "get_Os")]
+            get
+            {
+                return "Not " + Os;
+            }
+        }
+
+        public string Os
+        {
+            
+            [ShadowMethod(typeof(Computer), "get_Os")]
+            get
+            {
+                return null;
+            }
+        }
+        #endregion
+
+        #region 泛型方法
+        [RelocatedMethodAttribute(typeof(ComputerOf<string>), "ComputerIo")]
+        public string _impl_ComputerIo(string name)
+        {
+            var human = ComputerIo(name);
+            human = "Not " + human;
+            return human;
+        }
+        
+        [ShadowMethod(typeof(ComputerOf<string>), "ComputerIo")]
+        public string ComputerIo(string owner)
+        {
+            return null;
+        }
+        #endregion
+    }
+
+    public class NetFrameworkDetour : IMethodHook
+    {
+        [RelocatedMethodAttribute(typeof(System.IO.File), "ReadAllText")]
+        public static string _impl_ReadAllText(string file)
+        {
+            return ReadAllText(file) + "NetFrameworkDetour";
+        }
+
+        [ShadowMethod(typeof(System.IO.File), "ReadAllText")]
+        
+        public static string ReadAllText(string file)
+        {
+            return null;
         }
     }
 }
